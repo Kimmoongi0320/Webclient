2cec446d4a42c603a06affaf3e736432a3a92b1c
Kimmoongi0320 <84397561+Kimmoongi0320@users.noreply.github.com>
2024-05-08 12:59:29 +0900

    메뉴바 사진 추가 및 유저 db구성

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///members.db"
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String,nullable = False)
+    user_id = db.Column(db.String,nullable = False)
+    password = db.Column(db.String,nullable = False)
 
 @app.route("/")
 def home():
     return render_template("login_page.html")
 
 
-@app.route()
+@app.route('/sign_up')
 def sign_up():
-    pass
+    return render_template('sign_up_page.html')
 
 
 if __name__ == "__main__":
